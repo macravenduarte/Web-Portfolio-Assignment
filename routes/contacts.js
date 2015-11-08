41c932a15f13238537971787dce9dcd05dc8a1df
macravenduarte <duarte.marco242@gmail.com>
2015-11-08 01:09:51 -0500

    Bootstrap
    creating the layout for the page content for each. added links to my
    github repo, a couple github pictures and a location to receive peoples
    emails.

--- routes/contacts.js
+++ routes/contacts.js
@@ -10,7 +10,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
    //render the contact me index page with a title
     res.render('contacts/index', {
-       title: 'Contact Me' 
+        title: 'Contact Me'
+
     });
 });
 
