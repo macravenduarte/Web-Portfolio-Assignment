05d4b82c67045a99da71ecedc1c1b6989a1b57a1
macravenduarte <duarte.marco242@gmail.com>
2015-12-05 20:28:46 -0500

    reverting back to before adding passport
    Trying to correct issues with cloud serving and passport authenticating.
    reverting back to before the process started to try again

--- app.js
+++ app.js
@@ -6,25 +6,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//additions usd for AUTHENTICATION
-var mongoose = require('mongoose');
-var flash = require('connect-flash');
-var passport = require('passport');
-var session = require('express-session');
+//require MONGOOSE
+    var mongoose = require('mongoose');
 
-//route aliases
+//require CONTROLLERS
 //home page
-var routes = require('./routes/index');
+    var routes = require('./routes/index');
 //other pages
-var aboutMe = require('./routes/aboutMe');
-var contacts = require('./routes/contacts');
-var projects = require('./routes/projects');
-var services = require('./routes/services');
-var users = require('./routes/users');
+    var aboutMe = require('./routes/aboutMe');
+    var contacts = require('./routes/contacts');
+    var projects = require('./routes/projects');
+    var services = require('./routes/services');
 
 var app = express();
-
-require('./config/passport')(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,31 +32,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Session setup
-app.use(session({
-    secret: 'someSecret',
-    saveUninitialized: true,
-    resave: true
-}));
-
-//part of passport configuration
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
 //USE the pages you've created
-app.use('/', routes);
-app.use('/aboutMe', aboutMe);
-app.use('/contacts', contacts);
-app.use('/projects', projects);
-app.use('/services', services);
-app.use('/users', users);
+    app.use('/', routes);
+    app.use('/aboutMe', aboutMe);
+    app.use('/contacts', contacts);
+    app.use('/projects', projects);
+    app.use('/services', services);
 
 //Connect LOCALLY with mongoose
-mongoose.connect('mongodb://localhost/test');
+    //mongoose.connect('mongodb://localhost/test');
 
 //Connect LIVE
-//mongoose.connect('mongodb://<username>:<password>@ds048368.mongolab.com:48368/comp-2068');
+mongoose.connect('mongodb://<dbusername>:<dbPaassword>@ds048368.mongolab.com:48368/comp-2068');
                      
 //check DB connection
 var db = mongoose.connection;
@@ -70,12 +51,12 @@ var db = mongoose.connection;
     //if the connection fails to display an error message
     db.on('error', console.error.bind(console, 'DB Error: '));
     //if successful, the connection happens only once and prints confirmation to console
-    db.once('open', function (callback) {
+    db.once('open', function(callback) {
         console.log('Connected to mongodb');
     });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -97,7 +78,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
