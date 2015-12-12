3c5ea43b0cac99f04dfc63f1168640a339c43542
macravenduarte <duarte.marco242@gmail.com>
2015-12-11 22:17:35 -0500

    removed favicon
    completely removed the favicon

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //require the NODE MODULES
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -24,8 +23,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -43,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
     //mongoose.connect('mongodb://localhost/test');
 
 //Connect LIVE
-mongoose.connect('mongodb://duarte.marco:Raven$M0ngo@ds048368.mongolab.com:48368/comp-2068');
+mongoose.connect('mongodb://dbuser:dbpassword@ds048368.mongolab.com:48368/comp-2068');
                      
 //check DB connection
 var db = mongoose.connection;
