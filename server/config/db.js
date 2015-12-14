b2bc6896213c684a0665a5e4e6636fd819cdac39
macravenduarte <duarte.marco242@gmail.com>
2015-12-14 04:18:55 -0500

    passport, mongodb
    inital setup of views, routes and user model schema. pasge doesn't
    populate right yet but the db is able to connect locally and remotely.

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'url': 'mongodb://dbuser:dbuser@ds048368.mongolab.com:48368/comp-2068'
+};
+//Connect LOCALLY with mongoose
+//mongodb://localhost/test
+
+//Connect LIVE
+//mongodb://duarte.marco:Raven$M0ngo@ds048368.mongolab.com:48368/comp-2068
